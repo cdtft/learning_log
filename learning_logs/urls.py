a40d7b3865f69455ee9133a5047e7bfcd1dae8b8
wangcheng <cdtft007@gmail.com>
2023-04-18 16:33:58 +0800

    Django入门

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(r'', views.index, name='index')
+    # 主页
+    path("", views.index, name='index'),
+
+    # 显示所有主题
+    path("topics/", views.topics, name='topics'),
+
+    path("topics/<int:topic_id>/", views.topic, name='topic'),
 ]
