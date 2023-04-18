8b1648e299ff07b0808ea9b678694d6f443a5ad1
wangcheng <cdtft007@gmail.com>
2023-04-18 15:15:00 +0800

    模版学习

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from learning_logs.models import Topic, Entry
 
 # Register your models here.
+
+# 让Django通过管理网站管理我们的模型
+admin.site.register(Topic)
+admin.site.register(Entry)
